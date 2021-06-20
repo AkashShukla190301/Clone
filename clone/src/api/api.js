c28c084c97f128b09763e29b1dc5fe156f800592
aman <amanthapliyal14@gmail.com>
2021-06-20 12:25:26 +0530

    layout made

---
+++ clone/src/api/api.js
@@ -0,0 +1,9 @@
+// curl \
+//   'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=[YOUR_API_KEY]' \
+//   --header 'Authorization: Bearer [YOUR_ACCESS_TOKEN]' \
+//   --header 'Accept: application/json' \
+//   --compressed
+
+
+
+const apikey="AIzaSyD0KxgjX6s36QxG7jDp7GspgGR4-9YSfvc"
--- clone/src/components/app.js
+++ clone/src/components/app.js
@@ -1,12 +1,27 @@
 import React from "react";
+import './App.css'
 
 
 class App extends React.Component{
     render(){
        return(
-           <div>
-               <h1>this is Youtube</h1>
-           </div>
+        <div className="container">
+            <div className="topbar">
+                <h1>TopBAr</h1>
+            </div>
+
+            <div className="cards">
+                <h1>CArds</h1>
+                <h1>CArds</h1>
+                <h1>CArds</h1>
+                <h1>CArds</h1>
+                <h1>CArds</h1>
+            </div>
+
+            <div className="sidebar">
+                <h1>sidebar</h1>
+            </div>
+        </div>
        )
     }
 }
